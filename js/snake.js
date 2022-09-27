de8817027d8dd57aab5eaca035a8665c418a7fb8
blessyBBiju-Github <110586255+BlessyBBiju-github@users.noreply.github.com>
2022-09-27 18:22:34 +0530

    Files Committed

---
+++ js/snake.js
@@ -0,0 +1,33 @@
+ var player1 = "Player 1";
+    var sum=0; 
+    var num=1;
+    var current=0;
+    function editNames() {
+        player1 = prompt("Change Player1 name");
+        document.querySelector("p.Player1").innerHTML = player1;
+    }
+  
+    // Function to roll the dice
+    function rollTheDice() {
+             document.querySelector(".img1").setAttribute("src","Assets/dice.gif");
+    $("#"+sum+"").remove(":contains('A')");
+        setTimeout(function () {
+         current = Math.floor(Math.random() * 6) + 1;
+  
+            document.querySelector(".img1").setAttribute("src","assets/face" + current + ".png"); 
+   if(current==1){
+	num++;
+}
+   playGame();
+
+          
+        } ,1500);
+    }
+   function playGame(){
+       if(num>0){
+      sum+=current;
+      console.log(sum);	
+    $("#"+sum+"").append("A");
+
+}
+}
